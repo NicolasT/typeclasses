3cb53fc223ce5d71fbdc6e8835cb917fbbad7999
Nicolas Trangez <eikke@eikke.com>
2010-05-16 16:18:21 +0200

    Add Functor typeclass definition

---
+++ typeclasses/functor.py
@@ -0,0 +1,27 @@
+# typeclasses, an educational implementation of Haskell-style type
+# classes, in Python
+#
+# Copyright (C) 2010 Nicolas Trangez  <eikke eikke com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, version 2.1
+# of the License.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301  USA
+
+'''Definition of the Functor typeclass'''
+
+from typeclasses import function, TypeClass
+
+fmap = function(1)
+
+Functor = TypeClass(fmap)
